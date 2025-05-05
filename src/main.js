a4c746a0b3822152486521c41998b9ad3b6e50cb
zakisista <zakisista00@gmail.com>
2025-05-05 09:48:15 +0200

    valid input function

--- src/main.js
+++ src/main.js
@@ -9,16 +9,42 @@ document.querySelector('#app').innerHTML = `
     <button id="submit">submit</button>
   </div>
 `
+const attemptCount = 0
+
+const currentWord = 'hello'
+
+//2 checks, 1 for exact correctness, 2 for letter correctness excluding exact letters
 
 const submitButton = document.querySelector(`#submit`)
-const input = document.querySelector(`#input-text`)
-const outputText = document.querySelector(`#output-text`)
+const input        = document.querySelector(`#input-text`)
+const outputText   = document.querySelector(`#output-text`)
 
 const onSubmit = () => {
   console.log('been clicked')
   const inputContent = input.value
-  console.log(`text content = ${inputContent}`)
+
+  if (!isValidInput(inputContent)) {
+    return
+  }
+  for (let i=0; i<inputContent.length; i++) {
+    console.log(inputContent.slice(i, i+1))
+  }
+  outputText.innerHTML = inputContent.length
+  input.value = ''
 }
+
+const isValidInput = (inputText) => {
+  if (inputText === '') {
+    console.log('invalid input')
+    return false
+  }
+  if (inputText.length > 5) {
+    console.log('invalid input')
+    return false
+  }
+  return true
+}
+
 
 submitButton.addEventListener('click', onSubmit)
 
