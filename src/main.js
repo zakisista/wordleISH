60edbf031c4e64d2ea2c9e94e03a6ba47793b9d1
zakisista <zakisista00@gmail.com>
2025-05-05 09:27:17 +0200

    removed unnecessary boilerplate and added basic UI functions

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div id="output-text"></div>
+    <input id="input-text">
+    <button id="submit">submit</button>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+const submitButton = document.querySelector(`#submit`)
+const input = document.querySelector(`#input-text`)
+const outputText = document.querySelector(`#output-text`)
+
+const onSubmit = () => {
+  console.log('been clicked')
+  const inputContent = input.value
+  console.log(`text content = ${inputContent}`)
+}
+
+submitButton.addEventListener('click', onSubmit)
+
+
